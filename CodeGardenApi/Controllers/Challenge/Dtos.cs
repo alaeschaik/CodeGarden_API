765a5f6a561f6b98877e9f05340984cd6b461055
fkukcha <ferdaws_kukcha@yahoo.com>
2024-07-05 17:07:08 +0200

    Import Json Serialization namespace and leave ChallengeTypeDto

--- CodeGardenApi/Controllers/Challenge/Dtos.cs
+++ CodeGardenApi/Controllers/Challenge/Dtos.cs
@@ -5,7 +5,7 @@ namespace CodeGardenApi.Controllers.Challenge;
 
 public sealed record CreateChallengeDto(
     [property: SwaggerSchema(Nullable = false)]
-    ChallengeType ChallengeType,
+    ChallengeTypeDto ChallengeType,
     [property: SwaggerSchema(Nullable = false)]
     int SectionId,
     [property: SwaggerSchema(Nullable = false)]
@@ -13,21 +13,27 @@ public sealed record CreateChallengeDto(
 
 public sealed record UpdateChallengeDto(
     [property: SwaggerSchema(Nullable = true)]
-    ChallengeType? ChallengeType,
+    ChallengeTypeDto? ChallengeType,
     [property: SwaggerSchema(Nullable = true)]
     int? SectionId,
     [property: SwaggerSchema(Nullable = true)]
     string? Content);
 
+public enum ChallengeTypeDto
+{
+    MultipleChoice,
+    Question,
+    CodeSnippet,
+}
+
 public sealed record ChallengeDtoHelper
 {
-    public static ChallengeType ToChallengeTypeDto(ChallengeType? challengeType) =>
+    public static ChallengeType ToChallengeTypeDto(ChallengeTypeDto? challengeType) =>
         challengeType switch
         {
-            ChallengeType.MultipleChoice => ChallengeType.MultipleChoice,
-            ChallengeType.Question => ChallengeType.Question,
-            ChallengeType.CodeSnippet => ChallengeType.CodeSnippet,
-            ChallengeType.LearningContent => ChallengeType.LearningContent,
+            ChallengeTypeDto.MultipleChoice => ChallengeType.MultipleChoice,
+            ChallengeTypeDto.Question => ChallengeType.Question,
+            ChallengeTypeDto.CodeSnippet => ChallengeType.CodeSnippet,
             _ => throw new ArgumentOutOfRangeException(nameof(challengeType), challengeType, null),
         };
 }
--- CodeGardenApi/Program.cs
+++ CodeGardenApi/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.OpenApi.Models;
 using System.Text;
 using CodeGardenApi.ApplicationExtensions;
 using CodeGardenApi.Data;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
